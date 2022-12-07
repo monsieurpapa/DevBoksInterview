e0aef0596555163a2cbc3c8ab9b03f85b6e1afc8
Dieudo <dieudo@pop-os.localdomain>
2022-12-07 09:21:13 +0200

    added publisher model

--- publishers/models.py
+++ publishers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+    class Meta:
+        ordering = ["-name"]
+
+    def __str__(self):
+        return self.name
+
