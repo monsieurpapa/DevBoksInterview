c9f142c61d358abac0d1ddee75cf5b88f56bd04f
Dieudo <dieudo@pop-os.localdomain>
2022-12-07 14:20:38 +0200

    Point publisherlist view to current publishers directory

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,6 +19,7 @@ from django.urls import path , include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('publishers/', include('publishers.urls')),
     path("admin/", admin.site.urls),
     
 ]
