02ac3015bf1e8df5d958e33df03f1455331a345c
Dieudo <dieudo@pop-os.localdomain>
2022-12-07 09:23:53 +0200

    added Listview generic interface to list Publishers

--- publishers/views.py
+++ publishers/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from books.models import Publisher
 
 # Create your views here.
+class PublisherListView(ListView):
+    model = Publisher
