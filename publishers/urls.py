d4a9a238895f53f64d57218ee5d8a6eeeaa31370
Dieudo <dieudo@pop-os.localdomain>
2022-12-08 16:14:17 +0200

    url pattern for detailview successful

--- publishers/urls.py
+++ publishers/urls.py
@@ -1,10 +1,11 @@
 # urls.py
 from django.urls import path
-from publishers.views import PublisherListView, PublisherDetailView
+from publishers.views import PublisherListView, PublisherDetailView,PublisherCreateView
 
 app_name ='publishers'
 urlpatterns = [
     path('', PublisherListView.as_view(), name='publisher_list'),
-    path('<str:name>/', PublisherDetailView.as_view(), name='publisherdetail'), 
+    path('<int:pk>/', PublisherDetailView.as_view(), name='publisherdetail'), 
+    path('create/', PublisherCreateView.as_view(), name='publisher-create')
 
 ]
