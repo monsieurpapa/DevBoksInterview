6562dc10143f0022e2b43948bf8170c38fb5fb65
Dieudo <dieudo@pop-os.localdomain>
2022-12-08 14:17:52 +0200

    url pattern for detail view added, not working as expected

--- publishers/urls.py
+++ publishers/urls.py
@@ -1,7 +1,10 @@
 # urls.py
 from django.urls import path
-from publishers.views import PublisherListView
+from publishers.views import PublisherListView, PublisherDetailView
 
+app_name ='publishers'
 urlpatterns = [
-    path('', PublisherListView.as_view()),
+    path('', PublisherListView.as_view(), name='publisher_list'),
+    path('<str:name>/', PublisherDetailView.as_view(), name='publisherdetail'), 
+
 ]
