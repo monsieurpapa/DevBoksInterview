4ec1a643af800831b67444e9749185d65e44b1c4
Dieudo <dieudo@pop-os.localdomain>
2022-12-10 16:30:50 +0200

    using router class to determine the urlfconf automatically

--- publishers/urls.py
+++ publishers/urls.py
@@ -1,11 +1,22 @@
 # urls.py
 from django.urls import path
 from publishers.views import PublisherListView, PublisherDetailView,PublisherCreateView
+from django.contrib import admin
+from rest_framework import routers
+from publishers.views import PublisherViewSet
+
+
+''' rather than explicitly registering the views in a viewset in the urlconf, we register the viewset with a router class,
+ that automatically determines the urlconf .'''
+router = routers.DefaultRouter()
+router.register(r'publishers', PublisherViewSet, basename='publisher')
 
 app_name ='publishers'
 urlpatterns = [
     path('', PublisherListView.as_view(), name='publisher_list'),
     path('<int:pk>/', PublisherDetailView.as_view(), name='publisherdetail'), 
-    path('create/', PublisherCreateView.as_view(), name='publisher-create')
+    path('create/', PublisherCreateView.as_view(), name='publisher-create'),
+    path('admin/', admin.site.urls),
 
 ]
+urlpatterns += router.urls
