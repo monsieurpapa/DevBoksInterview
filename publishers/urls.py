7b80516ad3ff6dd6300c95bcfe023e587d399515
Dieudo <dieudo@pop-os.localdomain>
2022-12-13 04:16:29 +0200

    added routes to automatically generate url patterns for viewsets

--- publishers/urls.py
+++ publishers/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from publishers.views import PublisherListView, PublisherDetailView,PublisherCreateView
 from django.contrib import admin
 from rest_framework import routers
-from publishers.views import PublisherViewSet
+from publishers.viewsets import PublisherViewSet
 
 
 ''' rather than explicitly registering the views in a viewset in the urlconf, we register the viewset with a router class,
  that automatically determines the urlconf .'''
 router = routers.DefaultRouter()
-router.register(r'publishers', PublisherViewSet, basename='publisher')
+router.register(r'api/publishers', PublisherViewSet, basename='publisher')
 
 app_name ='publishers'
 urlpatterns = [
@@ -20,3 +20,6 @@ urlpatterns = [
 
 ]
 urlpatterns += router.urls
+
+# for url in router.urls:
+#     print(url ,"\n")
