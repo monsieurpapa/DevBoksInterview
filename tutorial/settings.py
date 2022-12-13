074f99792e65c8f5cc17065e62f0844d80ef5edb
Dieudo <dieudo@pop-os.localdomain>
2022-12-13 04:14:59 +0200

    commented out autentication params added to attempt the autentication issues faced

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -27,7 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+#authentifaction with REST_FRAMEWORK
+# REST_FRAMEWORK = {
+#    'DEFAULT_AUTHENTICATION_CLASSES': (
+#        'rest_framework.authentication.TokenAuthentication',
+#        'rest_framework.authentication.SessionAuthentication',
+#    ),
+#    'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAdminUser'
+#    ),
+# }
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    #"rest_framework.authtoken" #newly added
 ]
 
 MIDDLEWARE = [
